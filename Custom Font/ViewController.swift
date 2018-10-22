57226706ffd6ea9eefd549e787cf636eac840682
Serkan Özdemir <mserkanozdemir@gmail.com>
2018-10-22 21:29:36 +0300

    init

--- Custom Font/ViewController.swift
+++ Custom Font/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fonts = ["Helvetica Neue", "Verdana", "Courier", "Times New Roman", "Charter", "Georgia", "Noteworthy", "GillSans", "Menlo", "Farah", "Cochin", "Chalkduster"]
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBAction func changeFontButton(_ sender: Any) {
+        textLabel.font = UIFont(name: fonts.randomElement()!, size: 35)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
